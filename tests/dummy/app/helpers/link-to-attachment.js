a5a8a8c020aeb79cf1cd06611561cbd43a57d5c7
Justin Daniel <jdaniel@validusa.com>
2015-08-03 14:18:43 -0400

    Async helper.

--- tests/dummy/app/helpers/link-to-attachment.js
+++ tests/dummy/app/helpers/link-to-attachment.js
@@ -4,7 +4,13 @@ export default Ember.Helper.extend({
     globals: Ember.inject.service(),
     compute(params) {
         var aTagTemplate = "<a href='%@' target='_blank'>%@</a>",
-            url = Ember.String.fmt("%@/%@/%@", this.get("globals").get("host"), params[0].get("db"), params[0].get("id"));
+            url = Ember.String.fmt("%@/%@/%@", this.get("globals").get("host"), params[0].get("db"), params[0].get("id")),
+            self = this;
+        if (params[0].get("isLoading")) {
+            params[0]._internalModel._loadingPromise.then(function () {
+                self.recompute();
+            });
+        }
         return Ember.String.htmlSafe(Ember.String.fmt(aTagTemplate, url, params[0].get("file_name")));
     }
 });
